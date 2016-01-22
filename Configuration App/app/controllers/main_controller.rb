45be877eba21a613f0b76d2c8bbc0ee18b767955
Jason Burgett <jasbur@gmail.com>
2016-01-21 18:33:06 -0800

    Fixed more indenting

--- Configuration App/app/controllers/main_controller.rb
+++ Configuration App/app/controllers/main_controller.rb
@@ -1,29 +1,29 @@
 class MainController < ApplicationController
 
     def index
-        @current_values = Main.get_current_config_values
-        @wifi_ap_hash = Main.scan_wifi_networks
+      @current_values = Main.get_current_config_values
+      @wifi_ap_hash = Main.scan_wifi_networks
     end
 
     def save_credentials
-        ssid = params[:ap_info].split('+')[0]
-        encryption_type = params[:ap_info].split('+')[1]
-        
-        unless ssid.nil? or params[:wifi_key] == ""
-            Main.create_wpa_supplicant(ssid, encryption_type, params[:wifi_key])
-        end
+      ssid = params[:ap_info].split('+')[0]
+      encryption_type = params[:ap_info].split('+')[1]
 
-        unless params[:username] == "" or params[:password] == ""
-            Main.create_user_credential_file(params[:username], params[:password])
-        end
+      unless ssid.nil? or params[:wifi_key] == ""
+        Main.create_wpa_supplicant(ssid, encryption_type, params[:wifi_key])
+      end
 
-        if File.exist?('/etc/wpa_supplicant/wpa_supplicant.conf') and File.exist?('/home/pi/user_data/user_credentials')
-            Main.set_ap_client_mode
-        end
+      unless params[:username] == "" or params[:password] == ""
+        Main.create_user_credential_file(params[:username], params[:password])
+      end
+
+      if File.exist?('/etc/wpa_supplicant/wpa_supplicant.conf') and File.exist?('/home/pi/user_data/user_credentials')
+        Main.set_ap_client_mode
+      end
     end
 
     def reset_all
-        Main.reset_all
+      Main.reset_all
     end
 
 end
