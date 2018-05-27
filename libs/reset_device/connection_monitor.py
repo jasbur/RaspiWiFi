c91eee8f529e5d8dd2038880f6aa3469d2e81076
Jason Burgett <jasbur@gmail.com>
2018-05-27 14:48:40 -0500

    First attempt at pulling actual values for wifi status-based reset from /etc/rapiwifi/rapiwifi.conf

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -1,15 +1,20 @@
 import time
+import sys
 import reset_lib
 
 no_conn_counter = 0
+config_hash = reset_lib.config_file_hash()
 
-while True:
-    time.sleep(10)
+if config_hash['auto_config'] == 0:
+    sys.exit()
+else:
+    while True:
+        time.sleep(10)
 
-    if reset_lib.is_wifi_active() == False:
-        no_conn_counter = no_conn_counter + 10
-    else:
-        no_conn_counter = 0
+        if reset_lib.is_wifi_active() == False:
+            no_conn_counter = no_conn_counter + 10
+        else:
+            no_conn_counter = 0
 
-    if no_conn_counter >= 30:
-        reset_lib.reset_to_host_mode()
+        if no_conn_counter >= int(config_hash['auto_config_delay']):
+            reset_lib.reset_to_host_mode()
