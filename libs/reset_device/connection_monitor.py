d20fe1418215d1c5cdab56df9e74d012dd144694
Jason Burgett <jasbur@gmail.com>
2018-05-27 14:29:55 -0500

    Removed debigging code and added the actual trigger to reset to AP Host mode

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -7,11 +7,9 @@ while True:
     time.sleep(10)
 
     if reset_lib.is_wifi_active() == False:
-        print("No Connection for " + str(no_conn_counter) + " seconds...")
         no_conn_counter = no_conn_counter + 10
     else:
-        print("Connection is active...")
         no_conn_counter = 0
 
     if no_conn_counter >= 30:
-        print("Restting from inactivity...")
+        reset_lib.reset_to_host_mode()
