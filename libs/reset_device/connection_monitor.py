baba387a016df9dc495d4aaf7f3288a9cafd0564
Jason Burgett <jasbur@gmail.com>
2018-05-27 14:25:45 -0500

    Changed active connection checking to every 10 seconds instead of every 1 second.

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -4,11 +4,11 @@ import reset_lib
 no_conn_counter = 0
 
 while True:
-    time.sleep(1)
+    time.sleep(10)
 
     if reset_lib.is_wifi_active() == False:
         print("No Connection for " + str(no_conn_counter) + " seconds...")
-        no_conn_counter = no_conn_counter + 1
+        no_conn_counter = no_conn_counter + 10
     else:
         print("Connection is active...")
         no_conn_counter = 0
