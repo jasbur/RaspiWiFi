c13ce79f81d78cf09174a86a2730704acf860f6d
Jason Burgett <jasbur@gmail.com>
2018-05-27 14:10:37 -0500

    Added a couple debugging statements

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -5,10 +5,12 @@ no_conn_counter = 0
 
 while True:
     time.sleep(1)
-    
+
     if reset_lib.is_wifi_active() == False:
+        print("No Connection for " + no_conn_counter + " seconds...")
         no_conn_counter = no_conn_counter + 1
     else:
+        print("Connection is active...")
         no_conn_counter = 0
 
     if no_conn_counter >= 30:
