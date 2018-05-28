f3bb155141b5f61799dbd38d2bd3712371758778
Jason Burgett <jasbur@gmail.com>
2018-05-28 11:51:26 -0500

    Removed logging lines from debugging

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -16,11 +16,9 @@ else:
         if reset_lib.is_wifi_active() == False:
             no_conn_counter += 10
             consecutive_active_reports = 0
-            os.system('echo no conn counter: ' + str(no_conn_counter) + ' seconds >> /home/pi/connlog')
         else:
             consecutive_active_reports += 1
             no_conn_counter += 10
-            os.system('echo consecutive_active_reports: ' + str(consecutive_active_reports) + ' >> /home/pi/connlog')
             if consecutive_active_reports >= 2:
                 no_conn_counter = 0
                 consecutive_active_reports = 0
