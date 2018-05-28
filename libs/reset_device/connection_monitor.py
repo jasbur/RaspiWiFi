7531d7530121c0af810de0c9837ef983d202ba9e
Jason Burgett <jasbur@gmail.com>
2018-05-28 10:51:05 -0500

    Added a test for consecutive reports of an active connection (3) before resetting the no connection counter

--- libs/reset_device/connection_monitor.py
+++ libs/reset_device/connection_monitor.py
@@ -4,6 +4,7 @@ import os
 import reset_lib
 
 no_conn_counter = 0
+consecutive_active_reports = 0
 config_hash = reset_lib.config_file_hash()
 
 if config_hash['auto_config'] == "0":
@@ -14,10 +15,12 @@ else:
 
         if reset_lib.is_wifi_active() == False:
             no_conn_counter = no_conn_counter + 10
+            consecutive_active_reports = 0
             os.system('echo no conn counter: ' + str(no_conn_counter) + ' seconds >> /home/pi/connlog')
         else:
-            time.sleep(1)
-            if reset_lib.is_wifi_active() == True:
+            consecutive_active_reports = consecutive_active_reports + 1
+            os.system('echo consecutive_active_reports: ' + str(consecutive_active_reports) + ' >> /home/pi/connlog')
+            if consecutive_active_reports >= 3:
                 no_conn_counter = 0
 
         if no_conn_counter >= int(config_hash['auto_config_delay']):
