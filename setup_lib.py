328b3d938bb22e031b5128270cb7f28cdf8dd624
Jason Burgett <jasbur@gmail.com>
2018-06-11 10:19:00 -0500

    Added a bit of text to display while the Flask PIP installation is starting up. Before, it just sat at a blank screen for a slightly uncomfortable amount of time.

--- setup_lib.py
+++ setup_lib.py
@@ -6,6 +6,8 @@ def install_prereqs():
 	os.system('clear')
 	os.system('apt install python3 python3-rpi.gpio python3-pip dnsmasq hostapd -y')
 	os.system('clear')
+	print("Installing Flask web server...")
+	print()
 	os.system('pip3 install flask')
 	os.system('clear')
 
