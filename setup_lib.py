3ffec3f182c2e42a0f399a858b958806d6d0c5bb
Jason Burgett <jasbur@gmail.com>
2019-01-19 16:37:32 +0000

    Corrected faulty 'sed' command regarding WPA password

--- setup_lib.py
+++ setup_lib.py
@@ -40,7 +40,7 @@ def update_main_config_file(entered_ssid, auto_config_choice, auto_config_delay,
 		os.system('sed -i \'s/#wpa=2/wpa=2/\' /etc/hostapd/hostapd.conf')
 		os.system('sed -i \'s/#wpa_key_mgmt=WPA-PSK/wpa_key_mgmt=WPA-PSK/\' /etc/hostapd/hostapd.conf')
 		os.system('sed -i \'s/#rsn_pairwise=CCMP/rsn_pairwise=CCMP/\' /etc/hostapd/hostapd.conf')
-		os.system('sed -i \'s/#wpa_passphrase=somepassword/wpa_passphrase=somepassword' + wpa_entered_key + '/\' /etc/hostapd/hostapd.conf')
+		os.system('sed -i \'s/#wpa_passphrase=somepassword/wpa_passphrase=' + wpa_entered_key + '/\' /etc/hostapd/hostapd.conf')
 	if auto_config_choice.lower() == "y":
 		os.system('sed -i \'s/auto_config=0/auto_config=1/\' /etc/raspiwifi/raspiwifi.conf')
 	if auto_config_delay != "":
