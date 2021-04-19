72a72845ec2dbf90ce5a93a76ec4d05d78910ef1
Simon Horlick <simonhorlick@gmail.com>
2021-04-19 16:04:06 +0800

    Fix serial_last_four

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -8,13 +8,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
-serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
+serial_last_four = subprocess.getoutput(
+    "cat /proc/cpuinfo | grep Serial | awk '{print $3}'")[-4:]
 config_hash = reset_lib.config_file_hash()
 ssid_prefix = config_hash['ssid_prefix'] + " "
 reboot_required = False
 
 
-reboot_required = reset_lib.wpa_check_activate(config_hash['wpa_enabled'], config_hash['wpa_key'])
+reboot_required = reset_lib.wpa_check_activate(
+    config_hash['wpa_enabled'], config_hash['wpa_key'])
 
 reboot_required = reset_lib.update_ssid(ssid_prefix, serial_last_four)
 
