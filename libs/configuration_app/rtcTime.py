aefeacc6a87590ba90c7d9f2b33359bf7ea4223b
Fabian <fabianbarraez@gmail.com>
2021-07-21 16:55:04 -0500

    feat: 🎸 add system date set if RTC clock set does not work

--- libs/configuration_app/rtcTime.py
+++ libs/configuration_app/rtcTime.py
@@ -1,24 +1,30 @@
 import subprocess
+from datetime import datetime
+from time import time
+from timeRegister import Time_Register
 
 min_tries = 0
 max_tries = 5
 
+time_register = Time_Register('time_register.json')
+
 def read(): # read the time on the RTC module 
-    limitTimeArray = 19 # amount of characters to read on the string 
-    rtcTime = subprocess.check_output("sudo hwclock -r", shell = True)  # read the output of the rtc clock reading 
-    rtcTime = rtcTime[:limitTimeArray]  # save the target time 
-    return rtcTime      # return time saved
+    try:
+        limitTimeArray = 19 # amount of characters to read on the string 
+        rtcTime = subprocess.check_output("sudo hwclock -r", shell = True)  # read the output of the rtc clock reading 
+        rtcTime = rtcTime[:limitTimeArray]  # save the target time 
+        return rtcTime      # return time saved
+    except subprocess.CalledProcessError:   # if an error happens
+        today = datetime.now()
+        rtcTime = today.strftime("%Y-%m-%d %H:%M:%S")
+        return rtcTime      # return time saved
 
 def setSystClock(time): # set the system clock 
     timeGoal = str("sudo date -s " + '"' + time + '"')  # set string to input systemclock with time as parameter
+    RtcTimeGoal = str("sudo hwclock -w")  # write time into RTC module
     subprocess.call(timeGoal, shell = True)             # call process to set the systemclock time 
+    subprocess.call(RtcTimeGoal, shell = True)             # call process to set the systemclock time 
 
-# several attempts are necessary in order to avoid failures 
-for i in range(min_tries, max_tries):
-    try:
-        setSystClock(read())    # set the systems clock
-        break
-    except subprocess.CalledProcessError:   # if an error happens
-        print "trying again..."           
-        continue
+timeRead = read() # get the current time from rtc or system 
+setSystClock(timeRead)    # set the systems clock
     
