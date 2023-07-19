287891c9ef30f42253e57661cd6949fce92fde1e
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-19 13:59:21 -0500

    setup and teardown

--- uninstall.py
+++ uninstall.py
@@ -1,7 +1,7 @@
 import os
 
 os.system("systemctl disable dnsmasq")
-os.system("systemctl disable hostapd")
+os.system("systemctl disableg hostapd")
 
 os.system("cp /etc/dnsmasq.conf.copy /etc/dnsmasq.conf.conf")
 os.system("cp /etc/dhcpcd.conf.copy /etc/dhcpcd.conf")
